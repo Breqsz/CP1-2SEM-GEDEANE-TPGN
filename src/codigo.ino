6ed4cc6ac6eb7aca324d40daaa042a4e0e5543eb
Breqsz <rm97974>
2026-08-28 21:33:46 -0300

    Codigo v2

--- src/codigo.ino
+++ src/codigo.ino
@@ -1,80 +1,202 @@
+
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
 #include <DabbleESP32.h>
 
 #define IN1 16
 #define IN2 17
 #define IN3 18
 #define IN4 19
+
 #define ENA 5
-#define ENB 6
+#define ENB 23 
+
+
+#define TRIG_PIN 25
+#define ECHO_PIN 26
+
+
+#define DISTANCIA_SEGURANCA 20 // centímetros
+
+bool movendoParaFrente = false;
 
 void setup()
 {
+    Serial.begin(115200);
+
+
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
+
     pinMode(ENA, OUTPUT);
     pinMode(ENB, OUTPUT);
 
+
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
+
+    digitalWrite(ENA, HIGH);
+    digitalWrite(ENB, HIGH);
+
     Dabble.begin("My Bluetooth Car");
+
+    Serial.println("Carrinho Robo v0.2 iniciado!");
 }
 
 void loop()
 {
     Dabble.processInput();
 
-    GamePad.onPressed(GAMEPAD_UP, []()
-                      { moveForward(); });
+    float distancia = medirDistancia();
 
-    GamePad.onPressed(GAMEPAD_DOWN, []()
-                      { moveBackward(); });
+    Serial.print("Distancia: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
 
-    GamePad.onPressed(GAMEPAD_LEFT, []()
-                      { turnLeft(); });
 
-    GamePad.onPressed(GAMEPAD_RIGHT, []()
-                      { turnRight(); });
+    if (movendoParaFrente &&
+        distancia > 0 &&
+        distancia <= DISTANCIA_SEGURANCA)
+    {
+        stopMotors();
 
-    GamePad.onPressed(GAMEPAD_STOP, []()
-                      { stopMotors(); });
+        Serial.println("OBSTACULO DETECTADO!");
+
+        delay(50);
+        return;
+    }
+
+
+
+
+    if (GamePad.isUpPressed())
+    {
+        if (distancia > DISTANCIA_SEGURANCA || distancia <= 0)
+        {
+            moveForward();
+        }
+        else
+        {
+            stopMotors();
+        }
+    }
+
+    else if (GamePad.isDownPressed())
+    {
+        moveBackward();
+    }
+
+    else if (GamePad.isLeftPressed())
+    {
+        turnLeft();
+    }
+
+    else if (GamePad.isRightPressed())
+    {
+        turnRight();
+    }
+
+    else
+    {
+        stopMotors();
+    }
+
+    delay(30);
 }
+
+
+// SENSOR ULTRASSÔNICO
+
+
+float medirDistancia()
+{
+    // Garante pulso LOW antes da medição
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    // Envia pulso de 10 microssegundos
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(TRIG_PIN, LOW);
+
+    // Aguarda retorno do sinal
+    // Timeout de 30 ms para evitar travamento
+    long duracao = pulseIn(ECHO_PIN, HIGH, 30000);
+
+    // Caso nenhuma resposta seja recebida
+    if (duracao == 0)
+    {
+        return -1;
+    }
+
+    // Velocidade do som:
+    // distancia = tempo * 0.0343 / 2
+
+    float distancia = duracao * 0.0343 / 2;
+
+    return distancia;
+}
+
+// ========================================================
+// MOVIMENTAÇÃO
+// ========================================================
 
 void moveForward()
 {
+    movendoParaFrente = true;
+
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
+
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
 }
 
 void moveBackward()
 {
+    movendoParaFrente = false;
+
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
+
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
 }
 
 void turnLeft()
 {
+    movendoParaFrente = false;
+
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
+
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
 }
 
 void turnRight()
 {
+    movendoParaFrente = false;
+
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
+
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
 }
 
 void stopMotors()
 {
+    movendoParaFrente = false;
+
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
+
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
 }
